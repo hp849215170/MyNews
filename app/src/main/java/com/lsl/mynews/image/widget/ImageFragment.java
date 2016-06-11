c70dec8c81e66d2780f42a59c99f1d984fba0018
HP <huangpingyspa@163.com>
2016-06-11 23:28:57 +0800

    寿林，你个逗比，2016年6月11日23:28:44

--- app/src/main/java/com/lsl/mynews/image/widget/ImageFragment.java
+++ app/src/main/java/com/lsl/mynews/image/widget/ImageFragment.java
@@ -1,13 +1,20 @@
 package com.lsl.mynews.image.widget;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.lsl.mynews.R;
 import com.lsl.mynews.base.BaseFragment;
+import com.lsl.mynews.common.APIs;
+import com.lsl.mynews.util.L;
 
 /**
  * Description:
@@ -16,6 +23,12 @@ import com.lsl.mynews.base.BaseFragment;
  */
 public class ImageFragment extends BaseFragment {
 
+    private static ImageFragment instance;
+    private SwipeRefreshLayout imageRefresh;
+    private RecyclerView rvImage;
+    public static ImageFragment getInstance(){
+        return instance;
+    }
 
     @Nullable
     @Override
@@ -23,7 +36,30 @@ public class ImageFragment extends BaseFragment {
 
         View view = inflater.inflate(R.layout.fragment_image, container, false);
 
+        instance = this;
+
+        imageRefresh = (SwipeRefreshLayout) view.findViewById(R.id.image_refresh);
+        rvImage = (RecyclerView) view.findViewById(R.id.rv_image);
+
+        rvImage.setLayoutManager(new LinearLayoutManager(getActivity()));
+        imageRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                LoadImageResource.startLoading(APIs.IMAGES_URL);
+            }
+        });
         return view;
 
+    }
+
+    public Handler refreshHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            stopRefresh();
+        }
+    };
+
+    public void stopRefresh(){
+        imageRefresh.setRefreshing(false);
     }
 }
---
+++ app/src/main/java/com/lsl/mynews/image/widget/LoadImageResource.java
@@ -0,0 +1,51 @@
+package com.lsl.mynews.image.widget;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.lsl.mynews.bean.ImageBean;
+import com.lsl.mynews.common.HttpUtils;
+import com.lsl.mynews.common.ResultCallBack;
+import com.lsl.mynews.util.L;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+/**
+ * Created by HP on 2016/6/11.
+ * 读取图片资源
+ */
+public class LoadImageResource {
+
+    public static void startLoading(String url){
+        HttpUtils.get(url, new ResultCallBack() {
+            @Override
+            public void onSuccess(Response response) {
+                try {
+                    String imageJson = response.body().string();
+
+                    Gson gson = new Gson();
+                    JsonParser jsonParser = new JsonParser();
+                    JsonArray jsonArray = jsonParser.parse(imageJson).getAsJsonArray();
+                    for (int i = 0; i <jsonArray.size();i++){
+                        JsonElement jsonElement = jsonArray.get(i);
+                        ImageBean imageBean = gson.fromJson(jsonElement.getAsJsonObject(), ImageBean.class);
+                        L.i("ImageResource","success--->"+imageBean.getTitle());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                ImageFragment.getInstance().refreshHandler.sendEmptyMessage(0x001);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                L.i("ImageResource","onFailure--->"+e.getMessage());
+            }
+        });
+    }
+}
